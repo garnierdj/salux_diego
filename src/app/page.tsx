b1d01715908e174994c030f90da6db864acffea8
Diego Rodriguez Garnier <garnierdj@gmail.com>
2026-01-11 18:39:05 -0600

    Refactor Home component layout to include Navbar and Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import Navbar from "@/components/marketing/Navbar";
+import Footer from "@/components/marketing/Footer";
 import HomePageClient from "@/components/marketing/pages/HomePageClient";
 
 export default function Home() {
-  return <HomePageClient />;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <HomePageClient />
+      </main>
+      <Footer />
+    </div>
+  );
 }
