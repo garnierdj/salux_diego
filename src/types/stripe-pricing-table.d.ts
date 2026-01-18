482f9d902b11376b3e88366d48480be38229516d
garnierdj <garnierdj@gmail.com>
2026-01-17 18:48:37 -0600

    Fix JSX typing for Stripe pricing table

--- src/types/stripe-pricing-table.d.ts
+++ src/types/stripe-pricing-table.d.ts
@@ -1,10 +1,14 @@
 import type React from "react";
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    "stripe-pricing-table": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-      "pricing-table-id"?: string;
-      "publishable-key"?: string;
-    };
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "stripe-pricing-table": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        "pricing-table-id"?: string;
+        "publishable-key"?: string;
+      };
+    }
   }
 }
+
+export {};
